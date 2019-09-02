c95d9065d0f810e1c6b47ff3be99e8b788e1a33e
Ksenia Marchenko <ms.ksi2000@mail.ru>
2019-09-02 07:03:10 +0300

    Add function: map

--- libr_array.js
+++ libr_array.js
@@ -2,7 +2,16 @@ let MyArray={
     take(array, n){
         let newArray= array.slice(0,n);
         return newArray;
+    },
+    map(array, callback){
+        let newArray;
+        for(let i=0;i<array.length;i++){
+        newArray[i]=callback(array[i]);
+        }
+        return newArray;
     }
+
 }
 
 console.log(MyArray.take([1,2,3,4,5,6,7], 4));
+console.log(MyArray.map([1,2,3], a=>2*a));
