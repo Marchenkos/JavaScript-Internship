3abd98e949134f84385d60d93d977091ad5a5f73
Kseniya Marchanka <Kseniya.Marchanka@itechart-group.com>
2019-09-02 19:23:05 +0300

    Finish the memoized function

--- libr_array.js
+++ libr_array.js
@@ -1,134 +1,34 @@
-let MyArray={
 
-    array: [],
-
-    chain(array){
-     this.array=array;
-     return this;
-    },
-
-    take (n, arrayValue=this.array){
-        let newArray=[];
-        for(let i=0;i<n;i++)
-        {
-            newArray[i] = arrayValue[i];
-        }
-     if(this.array!=arrayValue)
-     {
-        this.array=newArray;
-        return this.array;
-     }
-        this.array=newArray;
-        return this;
-    },
-    map(callback,arrayValue=this.array){
-        let newArray=[];
-        for(let i=0;i<arrayValue.length;i++){
-            newArray[i]=callback(arrayValue[i]);
-        }
-        if(this.array!=arrayValue)
-        {
-           this.array=newArray;
-           return this.array;
-        }
-        this.array=newArray;
-        return this;
-    },
-    skip(n,arrayValue=this.array){
-        let newArray=[];
-
-        for(let i=n, j=0;i<arrayValue.length;i++, j++)
-        {
-            newArray[j]=arrayValue[i];
-        }
-        if(this.array!=arrayValue)
-        {
-           this.array=newArray;
-           return this.array;
-        }
-        this.array=newArray;
-
-        return this;
-    },
-    filter(callback,arrayValue=this.array){
-        let newArray=[];
-
-        let j=0;
-        for(let i=0;i<arrayValue.length;i++)
-        {
-            if(callback(arrayValue[i]))
-            {
-                newArray[j]=arrayValue[i];
-                j++;
-            }
-        }
-        if(this.array!=arrayValue)
-        {
-           this.array=newArray;
-           return this.array;
-        }
-        this.array=newArray;
-
-        return this;
-    },
-
-    foreach(callback,arrayValue=this.array){
-        let newArray=[];
-
-        for(let i=0;i<arrayValue.length;i++){
-            newArray[i]=callback(arrayValue[i]);
-        }
-        if(this.array!=arrayValue)
-        {
-           this.array=newArray;
-           return this.array;
-        }
-        this.array=newArray;
-
-        return this;
-    },
-    value(){
-        return this.array;
-    }
-
+function squared(array){
+    let newArray=[];
+  for(let i=0;i<array.length;i++){
+      newArray[i]=array[i]*array[i];
+  }
+    return newArray;
 }
 
-console.log(MyArray.chain([1,2,3]).take(2).map(a=>2*a).skip(1).value());
-console.log(MyArray.take(4,[1,2,3,4,5,6,7]));
-console.log(MyArray.map(a=>2*a,[1,2,3,4,5,6,7]));
-console.log(MyArray.skip( 2,[1,2,3,4,5,6,7]));
+const memoize = () => {
 
-var words = ['spray', 'limit', 'elite', 'exuberant', 'destruction', 'present'];
-console.log(MyArray.filter( word => word.length > 6,words));
-console.log(MyArray.foreach( a=>2*a,[1,2,3]));
+    let cacheValues= new Map();
 
-function q(array){
-    for(key in array)
-    {
-        array[key]=array[key]*array[key];
-    }
-    return array;
-}
+    return (array) => {
 
-const memoize = (fn)=>{
-    let cacheValues={};
-   return (...args)=>{
-    let n = args[0];  // тут работаем с единственным аргументом
-
-       if(n in cacheValues){
+       if(cacheValues.has('array')){
            console.log("from cache");
-           return cacheValues[n];
+           return cacheValues.get('array');
        }
        else{
-        let result=fn(n);
-        cacheValues[n]= result;
-                console.log("calculating");
- 
+        
+        console.log("calculating");
+        let result= squared(array);
+        cacheValues.set('array', result);
+      
+
         return result;
        }
 
    }
 }
-const memoizedAdd = memoize(q);
-console.log(memoizedAdd([1,2,3]));  // вычислено
-console.log(memoizedAdd([1,2,3]));  // взято из кэша
+const memoizedAdd = memoize();
+console.log(memoizedAdd([1,2,3]));  
+console.log(memoizedAdd([1,2,3]));  
