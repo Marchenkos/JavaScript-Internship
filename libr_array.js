a20eba4f160cd57038a8b4de8b459cc50487617b
Ksenia Marchenko <ms.ksi2000@mail.ru>
2019-09-03 10:33:26 +0300

    Code style modification

--- libr_array.js
+++ libr_array.js
@@ -1,32 +1,31 @@
+function squared(array) {
+    let newArray = [];
 
-function squared(array){
-    let newArray=[];
-  for(let i=0;i<array.length;i++){
-      newArray[i]=array[i]*array[i];
-  }
+    for(let i = 0; i < array.length; i++) {
+        newArray[i] = array[i] * array[i];
+    }
+
     return newArray;
 }
 
 const memoize = () => {
-
-    let cacheValues= new Map();
+    let cacheValues = new Map();
 
     return (array) => {
 
-       if(cacheValues.has('array')){
+       if(cacheValues.has('array')) {
            console.log("from cache");
+
            return cacheValues.get('array');
        }
-       else{
-        
-        console.log("calculating");
-        let result= squared(array);
-        cacheValues.set('array', result);
-      
 
-        return result;
+       else {
+           console.log("calculating");
+           let result = squared(array);
+           cacheValues.set('array', result);
+           
+           return result;
        }
-
    }
 }
 const memoizedAdd = memoize();
