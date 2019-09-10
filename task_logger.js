9a7f3cb94de9059f87f28230688d7082031ad26f
Kseniya Marchanka <Kseniya.Marchanka@itechart-group.com>
2019-09-10 17:58:29 +0300

    Change logger class

--- task_logger.js
+++ task_logger.js
@@ -2,57 +2,78 @@ class Logger {
   constructor(format = '[date]: [message]') {
     this.format = format;
     this.date = new Date();
-    this.vocabulary = {};
   }
-  _log(message, level, color) {
-    if(Array.isArray(message) || typeof message === 'object') {
-      this._format('', level, color);
-      console.table(message);
-    } else {
-      this._format(message, level, color);
-    }
-  }
-  _format(message, level, color) {
-    let dateString = this.convertDateToStrring(this.date);
-    let newFormat = this.format.replace('[date]', dateString).replace('[level]', level).replace('[message]', message);
-    console.log(`%c ${newFormat}`, color);
-  }
-  log(message) {
-    const color = '';
-    const level = '';
-    this.createVocabulary(message, level, color);
-    this._log(message, level, color);
-  }
-  info(message) {
-    const color = 'color: #0000FF';
-    const level = 'INFO';
-    this._log(message, level, color);
-  }
-  warning(message) {
-    const color = 'color: #ffc100';
-    const level = 'WARNING';
-    this._log(message, level, color);
-  }
-  error(message) {
-    const color = 'color: #ff1a00';
-    const level = 'ERROR';
-    this._log(message, level, color);
-  }
-  convertDateToStrring(date) {
-    return date.toLocaleString();
-  }
-  createVocabulary(message, level, color) {
+
+  _createVocabulary(message, level, color) {
+    let vocabulary = {};
     let listProperty  = this.format.replace(':', '').split(' ');
+    listProperty.push('[color]');
     let dateString = this.convertDateToStrring(this.date);
     let listValue = [dateString, level, message, color];
 
     listProperty.forEach((value, i) => {
-      this.vocabulary[value] = listValue[i];
+      vocabulary[value] = listValue[i];
     });
+
+    return vocabulary;
+  }
+
+  _log(message, vocabulary) {
+    if(Array.isArray(message) || typeof message === 'object') {
+      vocabulary['[message]'] = '';
+      this._format(vocabulary);
+      console.table(message);
+    } else {
+      this._format(vocabulary);
+    }
+  }
+
+  _format(vocabulary) {
+    let newFormat = this.format;
+
+    for(let key in vocabulary) {
+      newFormat = newFormat.replace(key, vocabulary[key]);
+    }
+
+    console.log(`%c ${newFormat}`, vocabulary['[color]']);
+  }
+
+  log(message) {
+    const color = '';
+    const level = '';
+    let vocabulary = this._createVocabulary(message, level, color);
+    this._log(message, vocabulary);
+  }
+
+  info(message) {
+    const color = 'color: #0000FF';
+    const level = 'INFO';
+    let vocabulary = this._createVocabulary(message, level, color);
+    this._log(message, vocabulary);
+  }
+
+  warning(message) {
+    const color = 'color: #ffc100';
+    const level = 'WARNING';
+    let vocabulary = this._createVocabulary(message, level, color);
+    this._log(message, vocabulary);
+  }
+
+  error(message) {
+    const color = 'color: #ff1a00';
+    const level = 'ERROR';
+    let vocabulary = this._createVocabulary(message, level, color);
+    this._log(message, vocabulary);
+  }
+
+  convertDateToStrring(date) {
+    return date.toLocaleString();
   }
 }
 
 let logger = new Logger('[date] [level]: [message]');
 logger.log('hello');
-logger.warning({'f':'sfs', 'sf':'sf'});
+logger.info('hello');
+logger.warning({'f':'bla', 's':'blabla'});
 logger.error('hello');
+logger.error({'f':'bla', 's':'blabla'});
