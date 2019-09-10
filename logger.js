d95c973970025f04f8f6c5a8703e7933d8cb26e7
Ksenia <43186251+Marchenkos@users.noreply.github.com>
2019-09-10 18:16:22 +0300

    Update logger.js

--- logger.js
+++ logger.js
@@ -3,21 +3,21 @@ class Logger {
       this.format = format;
       this.date = new Date();
     }
-  
+
     _createVocabulary(message, level, color) {
       let vocabulary = {};
       let listProperty  = this.format.replace(':', '').split(' ');
       listProperty.push('[color]');
       let dateString = this.convertDateToStrring(this.date);
       let listValue = [dateString, level, message, color];
-  
+
       listProperty.forEach((value, i) => {
         vocabulary[value] = listValue[i];
       });
-  
+
       return vocabulary;
     }
-  
+
     _log(message, vocabulary) {
       if(Array.isArray(message) || typeof message === 'object') {
         vocabulary['[message]'] = '';
@@ -27,53 +27,53 @@ class Logger {
         this._format(vocabulary);
       }
     }
-  
+
     _format(vocabulary) {
       let newFormat = this.format;
-  
+
       for(let key in vocabulary) {
         newFormat = newFormat.replace(key, vocabulary[key]);
       }
-  
+
       console.log(`%c ${newFormat}`, vocabulary['[color]']);
     }
-  
+
     log(message) {
       const color = '';
       const level = '';
       let vocabulary = this._createVocabulary(message, level, color);
       this._log(message, vocabulary);
     }
-  
+
     info(message) {
       const color = 'color: #0000FF';
       const level = 'INFO';
       let vocabulary = this._createVocabulary(message, level, color);
       this._log(message, vocabulary);
     }
-  
+
     warning(message) {
       const color = 'color: #ffc100';
       const level = 'WARNING';
       let vocabulary = this._createVocabulary(message, level, color);
       this._log(message, vocabulary);
     }
-  
+
     error(message) {
       const color = 'color: #ff1a00';
       const level = 'ERROR';
       let vocabulary = this._createVocabulary(message, level, color);
       this._log(message, vocabulary);
     }
-  
+
     convertDateToStrring(date) {
       return date.toLocaleString();
     }
-  }
+}
   
-  let logger = new Logger('[date] [level]: [message]');
-  logger.log('hello');
-  logger.info('hello');
-  logger.warning({'f':'bla', 's':'blabla'});
-  logger.error('hello');
-  logger.error({'f':'bla', 's':'blabla'});
+let logger = new Logger('[date] [level]: [message]');
+logger.log('hello');
+logger.info('hello');
+logger.warning({'f':'bla', 's':'blabla'});
+logger.error('hello');
+logger.error({'f':'bla', 's':'blabla'});
